2a0645dffc84656416450e1b27ce4dc54bf01778
ALTAIR <ily81420566@yandex.ru>
2017-08-13 21:35:45 +0300

    added and translated const

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+
         new OrderHandler(StandartMenu.getInstance()).start();
     }
 }
--- src/main/java/OrderHandler.java
+++ src/main/java/OrderHandler.java
@@ -7,8 +7,32 @@ public class OrderHandler {
     private boolean isFinish = false;
     private Scanner scanner = new Scanner(System.in);
 
+    private final String HELLO = "Hello! ";
+    private final String MAINMENU = "\n\nTo make an order, enter    E\n"  +
+            "\nTo print a report, enter   R\n" +
+            "\nTo exit the program, enter  Q\n";
+    private final String INCORRCOMMAND = "Incorrect command";
+    private final String EMPLOYEE = "Employee: ";
+    private final String TODAYMENU = "Today in the menu: ";
+    private final String PITY = "It's a pity, you didn't order anything";
+    private final String DISHNUM = "Enter the number of dish";
+    private final String RUB = "rub";
+    private final String THANKS = "Thanks for your order!";
+    private final String COST = "cost: ";
+    private final String WEIGHT = " weight: ";
+    private final String GRAMM = " gramm";
+    private final String ORDERCODE = "Order code: ";
+    private final String ORDERINFO = " Information about order: \n";
+    private final String TOTALCOST = "Total cost of the order: ";
+    private final String EMPTYLIST = "List of orders is empty!";
+    private final String ENTHERCODE = "Enter a code";
+    private final String ENTERNAME = "Please, enter your name\n";
+    private final String HEADOFREPORT = "Information about all orders for " +
+            "distribution of ordered dishes to employees:\n";
+    private final String ORDERINSTRUCTION ="Enter the order code of dishes one at a time. When done, enter X\n";
+
     public void start(){
-        System.out.println("Здравствуйте\n");
+        System.out.println(HELLO + "\n");
         showMenu();
 
         while (isFinish == false) {
@@ -25,9 +49,7 @@ public class OrderHandler {
 
         while (!isFinish) {
 
-            System.out.println("\n\nЧтобы сделать заказ введите   Е\n" +
-                    "Чтобы вывести отчет введите   R\n" +
-                    "Для выхода из программы введите   Q\n");
+            System.out.println(MAINMENU);
 
             String answer = scanner.nextLine().trim().toUpperCase();
 
@@ -38,7 +60,7 @@ public class OrderHandler {
             }else if(answer.equals("Q")){
                 isFinish = true;
             }else{
-                System.err.println("Неверная команда");
+                System.err.println(INCORRCOMMAND);
                 continue;
             }
 
@@ -65,78 +87,79 @@ public class OrderHandler {
 
     private void printGeneralReport() {
         if(!orders.isEmpty()) {
-            System.out.println("Информация о всех заказах для раздачи заказанных блюд сотрудникам: \n");
+            System.out.println(HEADOFREPORT);
             StringBuilder reportStr = new StringBuilder();
 
             for(Order order : orders) {
-                System.out.println("Заказчик: " + order.getEmployeeName());
+                System.out.println(EMPLOYEE + order.getEmployeeName());
                 for (Map.Entry<Dish, Integer> dish : order.getDishes().entrySet()) {
 
                     String name = makeName(dish.getKey().getName());
                     reportStr.append(name).append("x").append(dish.getValue()).append("\n");
 
                 }
-                reportStr.append("Общая стоимость заказа: ").append(order.getTotalCost()).append("руб")
+                reportStr.append(TOTALCOST).append(order.getTotalCost()).append(RUB)
                 .append("\n----------------");
                 System.out.println(reportStr);
                 reportStr.delete(0,reportStr.length());
             }
         }else {
-            System.out.println("Список заказов пуст!");
+            System.out.println(EMPTYLIST);
         }
 
     }
 
     private void printReportForDinningRoom(Order order) {
-        System.out.println("Информация о заказе: \n");
+        System.out.println(ORDERINFO);
         StringBuilder reportStr =  new StringBuilder();
         StringBuilder nameBuilder = new StringBuilder();
         int i = 0;
 
-        System.out.println("Заказчик: " + order.getEmployeeName());
+        System.out.println(EMPLOYEE + order.getEmployeeName());
         for(Map.Entry<Dish, Integer> dish : order.getDishes().entrySet()){
 
             String name = makeName(dish.getKey().getName());
 
-            reportStr.append(nameBuilder).append(" вес: ").append(dish.getKey()
-                    .getWeight()).append(" гр.").append("\tцена: ")
-                    .append(dish.getKey().getCost()).append("руб.\t")
+            reportStr.append(name).append(WEIGHT).append(dish.getKey()
+                    .getWeight()).append(GRAMM).append("\t").append(COST)
+                    .append(dish.getKey().getCost()).append(RUB).append("\t")
                     .append("x").append(dish.getValue()).append("\n");
 
             nameBuilder.delete(0,nameBuilder.length());
             i++;
         }
-        reportStr.append("Общая стоимость заказа: ").append(order.getTotalCost()).append("руб");
+        reportStr.append(TOTALCOST).append(order.getTotalCost()).append(RUB);
         System.out.println(reportStr);
 
     }
 
     private void createOrder() {
-        System.out.println("Пожалуйста, представтесь\n");
+        System.out.println(ENTERNAME);
         String employeeName;
 
         employeeName = scanner.nextLine();
 
-        System.out.println("Добрый день " + employeeName + "\n");
+        System.out.println(HELLO + employeeName + "\n");
 
         Order order =  new Order(employeeName);
 
-        System.out.println("Вводите код заказа блюд по одному. Когда закончите, введите X\n");
+        System.out.println(ORDERINSTRUCTION);
 
         boolean isContinue = true;
 
         while (isContinue){
-            System.out.println("Введите код\n");
+            System.out.println(ENTHERCODE + "\n");
             String answer = scanner.nextLine().toUpperCase().trim();
             if (answer.equals("X")){
                 if(!order.isEmpty()) {
                     orders.add(order);
-                    System.out.println("Спасибо за заказ!");
+                    System.out.println(THANKS);
                     printReportForDinningRoom(order);
                 }else {
-                    System.out.println("Очень жаль, вы ничего не заказали");
+                    System.out.println(PITY);
                 }
                 isContinue = false;
+                break;
             }
             try {
                 int dishNumber = Integer.parseInt(answer);
@@ -146,14 +169,14 @@ public class OrderHandler {
                     boolean isIncorrectInput = true;
 
                     while (isIncorrectInput) {
-                        System.out.println("Введите количество блюд\n");
+                        System.out.println(DISHNUM+ "\n");
                         answer = scanner.nextLine();
 
                          dishCount = Integer.parseInt(answer);
                          if (dishCount > 0){
                              isIncorrectInput = false;
                          }else {
-                             System.err.println("Неверное значение!!!");
+                             System.err.println(INCORRCOMMAND);
                          }
                     }
 
@@ -161,10 +184,10 @@ public class OrderHandler {
 
                     order.addDish(dish, dishCount);
                 }else{
-                    System.err.println("Неверное значение!");
+                    System.err.println(INCORRCOMMAND);
                 }
             }catch (Exception e){
-                System.err.println("Введите числовое значение");
+                System.err.println(INCORRCOMMAND);
             }
 
         }
@@ -173,7 +196,7 @@ public class OrderHandler {
     }
 
     private void showMenu(){
-        System.out.println("Сегодня в меню: \n");
+        System.out.println( TODAYMENU + "\n");
         StringBuilder menuStr =  new StringBuilder();
         StringBuilder nameBuilder = new StringBuilder();
         int i = 0;
@@ -182,8 +205,11 @@ public class OrderHandler {
 
             String name = makeName(dish.getName());
 
-            menuStr.append(nameBuilder).append(" вес: ").append(dish.getWeight()).append(" гр.").append("\tцена: ")
-                    .append(dish.getCost()).append("руб.\t").append("код заказа: ").append(i).append("\n");
+            menuStr.append(name).append(WEIGHT)
+                    .append(dish.getWeight()).append(GRAMM).append("\t").append(COST)
+                    .append(dish.getCost()).append(RUB).append("\t").append(ORDERCODE)
+                    .append(i).append("\n");
+
             nameBuilder.delete(0,60);
             i++;
         }
